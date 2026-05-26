d92272ee301b5b8912c877532d78483230ee2968
Pham Nguyen Thanh Tung <tungpnt9883@ut.edu.vn>
2026-05-26 14:45:40 +0700

    done demo database and tem registration API

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from .database import engine, Base
+from .routes import teams
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="SEAL Hackathon Management API")
+
+app.include_router(teams.router)
 
 @app.get("/")
 def read_root():
